002dc5ba81e5d908303c7d63dfdd2760f8453891
lihaoguang <991112>
2022-09-02 23:01:56 +0800

    整理代码

--- src/main/java/com/seven/matlab/controller/UserController.java
+++ src/main/java/com/seven/matlab/controller/UserController.java
@@ -2,15 +2,13 @@ package com.seven.matlab.controller;
 
 import com.seven.core.response.JSONResult;
 import com.seven.core.response.PageInfo;
-import com.seven.pojo.dto.UserDto;
 import com.seven.matlab.service.UserService;
+import com.seven.pojo.dto.UserDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 
 @Api(tags = "用户信息")
@@ -23,8 +21,8 @@ public class UserController {
     private UserService service;
 
     @ApiOperation(value = "添加")
-    @RequestMapping(value = "save",method = RequestMethod.POST)
-    public JSONResult save(@RequestBody UserDto dto)throws Exception{
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public JSONResult save(@RequestBody UserDto dto) throws Exception {
         try {
             service.save(dto);
             return new JSONResult().setSuccess(true);
@@ -35,8 +33,8 @@ public class UserController {
     }
 
     @ApiOperation(value = "删除")
-    @RequestMapping(value = "delete/{id}",method = RequestMethod.POST)
-    public JSONResult delete(@PathVariable(value = "id")String id)throws Exception{
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.POST)
+    public JSONResult delete(@PathVariable(value = "id") String id) throws Exception {
         try {
             service.delete(id);
             return new JSONResult().setSuccess(true);
@@ -47,8 +45,8 @@ public class UserController {
     }
 
     @ApiOperation(value = "修改")
-    @RequestMapping(value = "update",method = RequestMethod.POST)
-    public JSONResult update(@RequestBody UserDto dto)throws Exception{
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public JSONResult update(@RequestBody UserDto dto) throws Exception {
         try {
             service.update(dto);
             return new JSONResult().setSuccess(true);
@@ -59,8 +57,8 @@ public class UserController {
     }
 
     @ApiOperation(value = "查询")
-    @RequestMapping(value = "query",method = RequestMethod.POST)
-    public JSONResult query(PageInfo pageInfo)throws Exception{
+    @RequestMapping(value = "query", method = RequestMethod.POST)
+    public JSONResult query(PageInfo pageInfo) throws Exception {
         try {
             PageInfo info = service.query(pageInfo);
             return new JSONResult().setSuccess(true).setResult(info);
